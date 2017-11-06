338ac2ef140ab73fd9c5df776f9e034d3d2980da
xquadradpi <aa111088>
2017-11-06 12:51:20 +0100

    added RoomOwner and Prepare UserCollection to join room

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Kickstart your Meteor-Angular Project
 
 ### Install Meteor
+
+To run and install Meteor -> Node / npm needs to be installed.
+
 ```
 $ curl https://install.meteor.com | sh
 ```
@@ -17,4 +20,31 @@ $ meteor npm install
 ### Run Project
 ```
 $ meteor
+```
+
+### Commands
+
+Run Meteor
+``` 
+$ meteor run
+```
+Run Meteor in Production Mode
+``` 
+$ meteor run --production
+```
+Reset Meteor including MongoDB
+``` 
+$ meteor reset
+```
+Update all Packages
+``` 
+$ meteor update --all-packages
+```
+Build Meteor App
+```
+$ meteor build ./build/
+```
+Start Tests
+```
+$ meteor test --driver-package practicalmeteor:mocha
 ```
--- client/imports/app/board/board.component.ts
+++ client/imports/app/board/board.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, NgZone, OnInit } from "@angular/core";
+import { Meteor } from 'meteor/meteor';
+
+import { User } from '../../../../both/models/user.model';
 
 import template from './board.component.html';
 import style from './board.scss';
@@ -11,9 +14,41 @@ import style from './board.scss';
 
 export class BoardComponent implements OnInit {
 
-    constructor(){}
+    userId: string;
+    loadUser: boolean;
+    hasRoom: boolean;
+    user: User;
+
+    constructor(
+        private zone: NgZone
+    ){}
 
     ngOnInit() {
+
+        this.userId = Meteor.userId();
+
+        if(this.userId) {
+
+            Tracker.autorun(() => {
+                this.zone.run(() =>{
+
+                    this.loadUser = false;
+                    this.hasRoom = false;
+
+                    this.user = Meteor.users.findOne(this.userId);
+                    if(typeof this.user !== typeof undefined) {
+
+                        this.loadUser = true;
+
+                        if(this.user.room){
+                            this.hasRoom = true;
+                        }
+                    }
+
+                })
+            });
+        }
+
 
     }
 }
